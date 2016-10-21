b1cd9f32bd93ce15db5efdc61dac65aec84c64a7
matianming <m324539017@gmail.com>
2016-10-21 13:29:40 +0800

    first commit

---
+++ EslintAutoFix.py
@@ -0,0 +1,36 @@
+import sublime, sublime_plugin
+
+SETTINGS_KEY = 'EslintAutoFix.sublime-settings'
+DEFAULT_ESLINT_PATH = ''
+DEFAULT_SHOW_PANEL = True
+
+class Preferences:
+  def load(self, settings):
+    self.eslint_path = settings.get('eslint_path', DEFAULT_ESLINT_PATH)
+    self.show_panel = settings.get('show_panel', DEFAULT_SHOW_PANEL)
+
+Pref = Preferences()
+
+def plugin_loaded():
+  settings = sublime.load_settings(SETTINGS_KEY)
+  Pref.load(settings)
+  settings.add_on_change('reload', lambda: Pref.load(settings))
+
+class Eslint_auto_fixCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		fileName = self.window.active_view().file_name();
+		args = {
+		  'cmd': [
+		    'eslint',
+		    '--fix',
+		    '--no-ignore',
+		    fileName
+		  ],
+		  'path': Pref.eslint_path
+		}
+		self.window.run_command('exec', args)
+		if not Pref.show_panel:
+		  self.window.run_command("hide_panel", {"panel": "output.exec"})
+	
+
+
--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # eslintAutoFix
-a sublime plugin
+
+A sublime plugin.
+
+## setting
+
+### eslint_path
+
+* Global installation path of eslint.
+
+### show_panel
+
+* Whether the output panel, default true.
+
+
+e.g.
+
+```
+{
+  "eslint_path": "/Users/matianming/nvm/versions/node/v4.2.4/bin/",
+  "show_panel": true
+}
+
+```
+
+## command
+
+### `eslint_auto_fix`
+
+## keymap
+
+### `super+shift+,`
